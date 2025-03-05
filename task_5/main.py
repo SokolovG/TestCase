48ce23f452af65e54a04ed626e3ccd9e5f58ce2e
SokolovG <griso2010@yandex.ru>
2025-03-05 13:16:36 +0200

    Small logic in analyze_numbers changed

--- task_5/main.py
+++ task_5/main.py
@@ -7,7 +7,7 @@ logger = Logger("Task_5")
 input_list: Final[list[int]] = [10, 20, 30, 40, 50, 30, 20]
 
 
-def analyze_numbers(numbers: list[int]) -> dict[int | list[int]]:
+def analyze_numbers(numbers: list[int]) -> dict[str, int | list[int]]:
     """Анализирует список чисел и возвращает статистику.
 
     Args:
@@ -20,14 +20,14 @@ def analyze_numbers(numbers: list[int]) -> dict[int | list[int]]:
             - "Числа, делящиеся на 3": список всех чисел, которые делятся на 3
     """
     result = {}
-    unique_numbers = len(set(numbers))
-    divisible_by_three = [num for num in numbers if num % 3 == 0]
-    max_num = max(numbers)
-    numbers.pop(numbers.index(max(numbers)))
+    numbers_copy = numbers.copy()
 
-    result["Уникальные числа"] = unique_numbers
-    result["Второе по величине число"] = max(numbers)
-    result["Числа, делящиеся на 3"] = divisible_by_three
+    result["Числа, делящиеся на 3"] = [num for num in numbers if num % 3 == 0]
+    result["Уникальные числа"] = len(set(numbers))
+
+    sorted_unique = sorted(set(numbers_copy), reverse=True)
+    result["Второе по величине число"] = sorted_unique[1] if len(sorted_unique) > 1 else None
+
 
     return result
 
--- task_7/main.py
+++ task_7/main.py
@@ -22,26 +22,33 @@ def list_products_less_10() -> list[tuple[str]]:
         return []
 
 
-def update_price_by_name(new_price: int, name: str) -> None:
+def update_price_by_name(new_price: int, name: str) -> bool:
     """Обновляет цену продукта по его названию.
 
     Args:
        new_price: Новая цена продукта. Должна быть неотрицательной.
        name: Название продукта, цену которого нужно обновить.
+    Returns:
+        True/False
     """
     if new_price < 0:
         logger.error("Цена не может быть отрицательной")
-        return
+        return False
+
+    if not name:
+        logger.error("Название продукта не может быть пустым")
+        return False
 
     try:
         with get_cursor() as cur:
             cur.execute(
                 "UPDATE products SET price = %s WHERE name = %s", (new_price, name)
             )
+            logger.success(f"Обновлена цена {name} на {new_price}")
 
     except Exception as e:
         logger.error(f"Ошибка при обновлении цены: {e}")
-        return
+        return False
 
 
 def main() -> None:
